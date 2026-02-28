91ce52e24251c0367aed6689ec1ad218eca78b86
thirupathimech <thirupathi7845@gmail.com>
2026-02-28 10:08:06 +0530

    api disable in docker

---
+++ src/main/java/com/aadhik/ecommerce/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.aadhik.ecommerce;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
